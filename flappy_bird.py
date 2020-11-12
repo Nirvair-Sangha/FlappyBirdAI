fe7c48b933e595ac6e72f0fd0330620cd69d4f86
Nirvair Sangha <nirvairsangha13@gmail.com>
2020-11-12 02:05:20 -0600

    finished AI project

--- flappy_bird.py
+++ flappy_bird.py
@@ -3,9 +3,12 @@ import neat
 import time
 import os
 import random
+pygame.font.init() #gets font
 
 WIN_WIDTH = 600
 WIN_HEIGHT = 800
+
+GEN = 0
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
              pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
@@ -13,6 +16,9 @@ BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bi
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
+
 
 class Bird:
     IMGS = BIRD_IMGS
@@ -115,7 +121,7 @@ class Pipe:
     def collide(self, bird):
         bird_mask = bird.get_mask()#masks are what surround sprites to help determine collisions
         top_mask = pygame.mask.from_surface(self.PIPE_TOP)
-        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+        bottom_mask = pygame.mask.from_surface(self.PIP_BOTTOM)
 
         top_offset = (self.x - bird.x, self.top - round(bird.y)) #no decimals so round bird off
         bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
@@ -154,44 +160,142 @@ class Base:
         win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(win, bird, pipes, base):
+def draw_window(win, birds, pipes, base, score, gen):
     win.blit(BG_IMG, (0,0))#blit means to draw to window
     for pipe in pipes: #pipes is a list due to multiple pipes appearing on screen at once
         pipe.draw(win)
 
-    base.draw(win)
+    text = STAT_FONT.render("Score: " + str(score), 1, (255,255,255)) # the color of font
+    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
 
-    bird.draw(win)
+    text = STAT_FONT.render("Generation: " + str(gen), 1, (255, 255, 255))  # the color of font
+    win.blit(text, (10,10))
+
+    base.draw(win)
+    for bird in birds:
+        bird.draw(win)
+
     pygame.display.update()
 
 
-def main():
-    bird = Bird(230,350) #create bird object
+def main(genomes, config):
+    global GEN
+    GEN += 1
+    nets = []
+    ge = []
+    birds = [] #create birds list for multiple birds
+
+    for _,g in genomes: #tuple that has genome id which we ignore
+        net  = neat.nn.FeedForwardNetwork.create(g, config)#makes the NN
+        nets.append(net)
+        birds.append(Bird(230, 350))
+        g.fitness = 0
+        ge.append(g)
+
+
     base = Base(730)#bottom of screen
-    pipes = [Pipe(700)]
+    pipes = [Pipe(600)] #spawns pipes closer
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT)) #shorthand of window
     clock = pygame.time.Clock()
 
+    score = 0
+
     run = True
     while run:
-        clock.tick(30)
+        clock.tick(30)#make it much faster without rendering to screen
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
+                quit()
+
+        pipe_ind = 0
+        if len(birds) > 0: #get first bird in x position it doesnt matter
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+                pipe_ind = 1
+        else:
+            run = False
+            break
+
+        for x, bird in enumerate(birds): # every second the bird stays alive it gains a fitnes point
+            bird.move()
+            ge[x].fitness += 0.1
+
+            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height),
+                                       abs(bird.y - pipes[pipe_ind].bottom)))
+
+            if output[0] > 0.5:
+                bird.jump()
 
         #bird.move()
+        add_pipe = False
+        rem = []
         for pipe in pipes:
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x].fitness -= 1 #bird incentive not to hit the pipe
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
+
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+
             pipe.move()
 
+        if add_pipe:
+            score += 1
+            for g in ge:
+                g.fitness += 5 #go through pipes rather than making it farther in level
+            pipes.append(Pipe(600)) #change this to smaller number for closer spawns
+
+        for r in rem:
+            pipes.remove(r)
+
+        for x, bird in enumerate(birds):
+            if bird.y + bird.img.get_height() >= 730 or bird.y < 0: #hit floor and lost
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
+
         base.move()
-        draw_window(win, bird, pipes, base)
-
-    pygame.quit()
-    quit()
-
-main()
+        draw_window(win, birds, pipes, base, score, GEN)
 
 
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                config_path)
+
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main,50)
+
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt") #finds the absolute path
+    run(config_path)
+
+'''
+Information to give NEAT algo:
+Inputs: Bird y, Top pipe, Bottom pipe only need one pipe but both speed it up
+Output: Jump or Dont jump
+Activation Function(): TanH (more negative closer to -1 and more positive closer to 1) if val is greater than 0.5 then jump
+Sigmoid is also a popular activation function ()
+Population Size: 100 birds in each generation and keep breeeding best birds until only the best remain
+The larger the population size the better to breed the best birds
+Fitness Function: Determines which birds are better, whatever bird gets the furthest is the best
+Max Generations: 30, if you dont get the perfect bird by 30, just stop and try again
+'''
 
 
 
